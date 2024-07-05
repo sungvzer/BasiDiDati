22d90b0433d9f3b27c1415f48ddaa8ef55360a3f
sungvzer <sungvzer@proton.me>
2024-07-06 01:03:06 +0200

    fix: add default timestamp

--- up/103-Organizzazione_Campionato.sql
+++ up/103-Organizzazione_Campionato.sql
@@ -2,6 +2,6 @@ CREATE TABLE Organizzazione_Campionato (
   p_iva_organizzatore VARCHAR(11),
   nome_campionato VARCHAR(100),
   anno_campionato NUMBER,
-  created_at timestamp,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (p_iva_organizzatore, nome_campionato, anno_campionato)
 );
