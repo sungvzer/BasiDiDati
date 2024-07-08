6228c92e2bbfbf130bbcdaa69e94174b282c09bb
sungvzer <sungvzer@proton.me>
2024-07-08 08:04:00 +0200

    fix: add generated columns into inserts for FK references

--- up/310-Sessioni.sql
+++ up/310-Sessioni.sql
@@ -1,74 +1,45 @@
--- Formula 1 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 90, TO_DATE('2024-03-29 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 1', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 60, TO_DATE('2024-03-30 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 1', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 120, TO_DATE('2024-03-31 13:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 1', 2024);
-
--- Formula 2 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 45, TO_DATE('2024-03-29 08:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 2', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2024-03-30 11:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 2', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 60, TO_DATE('2024-03-31 11:30:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 2', 2024);
-
--- Formula 3 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 45, TO_DATE('2024-03-29 07:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 3', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 30, TO_DATE('2024-03-30 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 3', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 45, TO_DATE('2024-03-31 09:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula 3', 2024);
-
--- Formula E 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 30, TO_DATE('2024-04-05 09:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula E', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2024-04-06 13:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula E', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 60, TO_DATE('2024-04-07 15:00:00','yyyy-mm-dd hh24:mi:ss'), 'Formula E', 2024);
-
--- Super GT 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 60, TO_DATE('2024-02-16 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Super GT', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2024-02-17 13:00:00','yyyy-mm-dd hh24:mi:ss'), 'Super GT', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 90, TO_DATE('2024-02-18 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Super GT', 2024);
-
--- Campionato Italiano di Drift 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 45, TO_DATE('2024-05-10 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano di Drift', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 30, TO_DATE('2024-05-11 16:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano di Drift', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 60, TO_DATE('2024-05-12 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano di Drift', 2024);
-
--- Campionato Italiano di Drift 2025
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 45, TO_DATE('2025-05-09 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano di Drift', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 30, TO_DATE('2025-05-10 16:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano di Drift', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 60, TO_DATE('2025-05-11 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano di Drift', 2025);
-
--- Endurance 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 90, TO_DATE('2024-01-05 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Endurance', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 60, TO_DATE('2024-01-06 12:00:00','yyyy-mm-dd hh24:mi:ss'), 'Endurance', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 180, TO_DATE('2024-01-07 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Endurance', 2024);
-
--- Endurance 2025
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 90, TO_DATE('2025-01-03 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Endurance', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 60, TO_DATE('2025-01-04 12:00:00','yyyy-mm-dd hh24:mi:ss'), 'Endurance', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 180, TO_DATE('2025-01-05 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Endurance', 2025);
-
--- Campionato Italiano GT 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 60, TO_DATE('2024-03-01 09:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano GT', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2024-03-02 11:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano GT', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 90, TO_DATE('2024-03-03 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano GT', 2024);
-
--- Campionato Italiano GT 2025
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 60, TO_DATE('2025-03-07 09:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano GT', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2025-03-08 11:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano GT', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 90, TO_DATE('2025-03-09 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano GT', 2025);
-
--- Campionato Italiano Turismo 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 45, TO_DATE('2024-04-05 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano Turismo', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 30, TO_DATE('2024-04-06 13:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano Turismo', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 60, TO_DATE('2024-04-07 15:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano Turismo', 2024);
-
--- Campionato Italiano Turismo 2025
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 45, TO_DATE('2025-04-04 10:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano Turismo', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 30, TO_DATE('2025-04-05 13:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano Turismo', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 60, TO_DATE('2025-04-06 15:00:00','yyyy-mm-dd hh24:mi:ss'), 'Campionato Italiano Turismo', 2025);
-
--- EURO GT4 2024
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 60, TO_DATE('2024-03-01 09:00:00','yyyy-mm-dd hh24:mi:ss'), 'EURO GT4', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2024-03-02 11:00:00','yyyy-mm-dd hh24:mi:ss'), 'EURO GT4', 2024);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 90, TO_DATE('2024-03-03 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'EURO GT4', 2024);
-
--- EURO GT4 2025
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('prove_libere', 60, TO_DATE('2025-03-07 09:00:00','yyyy-mm-dd hh24:mi:ss'), 'EURO GT4', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('qualifica', 45, TO_DATE('2025-03-08 11:00:00','yyyy-mm-dd hh24:mi:ss'), 'EURO GT4', 2025);
-INSERT INTO Sessione (tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES ('gara', 90, TO_DATE('2025-03-09 14:00:00','yyyy-mm-dd hh24:mi:ss'), 'EURO GT4', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(1, 'prove_libere', 90, TIMESTAMP '2024-03-29 10:00:00.000000', 'Formula 1', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(2, 'qualifica', 60, TIMESTAMP '2024-03-30 14:00:00.000000', 'Formula 1', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(3, 'gara', 120, TIMESTAMP '2024-03-31 13:00:00.000000', 'Formula 1', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(4, 'prove_libere', 45, TIMESTAMP '2024-03-29 08:00:00.000000', 'Formula 2', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(5, 'qualifica', 45, TIMESTAMP '2024-03-30 11:00:00.000000', 'Formula 2', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(6, 'gara', 60, TIMESTAMP '2024-03-31 11:30:00.000000', 'Formula 2', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(7, 'prove_libere', 45, TIMESTAMP '2024-03-29 07:00:00.000000', 'Formula 3', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(8, 'qualifica', 30, TIMESTAMP '2024-03-30 10:00:00.000000', 'Formula 3', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(9, 'gara', 45, TIMESTAMP '2024-03-31 09:00:00.000000', 'Formula 3', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(10, 'prove_libere', 30, TIMESTAMP '2024-04-05 09:00:00.000000', 'Formula E', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(11, 'qualifica', 45, TIMESTAMP '2024-04-06 13:00:00.000000', 'Formula E', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(12, 'gara', 60, TIMESTAMP '2024-04-07 15:00:00.000000', 'Formula E', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(13, 'prove_libere', 60, TIMESTAMP '2024-02-16 10:00:00.000000', 'Super GT', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(14, 'qualifica', 45, TIMESTAMP '2024-02-17 13:00:00.000000', 'Super GT', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(15, 'gara', 90, TIMESTAMP '2024-02-18 14:00:00.000000', 'Super GT', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(16, 'prove_libere', 45, TIMESTAMP '2024-05-10 14:00:00.000000', 'Campionato Italiano di Drift', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(17, 'qualifica', 30, TIMESTAMP '2024-05-11 16:00:00.000000', 'Campionato Italiano di Drift', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(18, 'gara', 60, TIMESTAMP '2024-05-12 14:00:00.000000', 'Campionato Italiano di Drift', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(19, 'prove_libere', 45, TIMESTAMP '2025-05-09 14:00:00.000000', 'Campionato Italiano di Drift', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(20, 'qualifica', 30, TIMESTAMP '2025-05-10 16:00:00.000000', 'Campionato Italiano di Drift', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(21, 'gara', 60, TIMESTAMP '2025-05-11 14:00:00.000000', 'Campionato Italiano di Drift', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(22, 'prove_libere', 90, TIMESTAMP '2024-01-05 10:00:00.000000', 'Endurance', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(23, 'qualifica', 60, TIMESTAMP '2024-01-06 12:00:00.000000', 'Endurance', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(24, 'gara', 180, TIMESTAMP '2024-01-07 14:00:00.000000', 'Endurance', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(25, 'prove_libere', 90, TIMESTAMP '2025-01-03 10:00:00.000000', 'Endurance', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(26, 'qualifica', 60, TIMESTAMP '2025-01-04 12:00:00.000000', 'Endurance', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(27, 'gara', 180, TIMESTAMP '2025-01-05 14:00:00.000000', 'Endurance', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(28, 'prove_libere', 60, TIMESTAMP '2024-03-01 09:00:00.000000', 'Campionato Italiano GT', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(29, 'qualifica', 45, TIMESTAMP '2024-03-02 11:00:00.000000', 'Campionato Italiano GT', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(30, 'gara', 90, TIMESTAMP '2024-03-03 14:00:00.000000', 'Campionato Italiano GT', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(31, 'prove_libere', 60, TIMESTAMP '2025-03-07 09:00:00.000000', 'Campionato Italiano GT', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(32, 'qualifica', 45, TIMESTAMP '2025-03-08 11:00:00.000000', 'Campionato Italiano GT', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(33, 'gara', 90, TIMESTAMP '2025-03-09 14:00:00.000000', 'Campionato Italiano GT', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(34, 'prove_libere', 45, TIMESTAMP '2024-04-05 10:00:00.000000', 'Campionato Italiano Turismo', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(35, 'qualifica', 30, TIMESTAMP '2024-04-06 13:00:00.000000', 'Campionato Italiano Turismo', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(36, 'gara', 60, TIMESTAMP '2024-04-07 15:00:00.000000', 'Campionato Italiano Turismo', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(37, 'prove_libere', 45, TIMESTAMP '2025-04-04 10:00:00.000000', 'Campionato Italiano Turismo', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(38, 'qualifica', 30, TIMESTAMP '2025-04-05 13:00:00.000000', 'Campionato Italiano Turismo', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(39, 'gara', 60, TIMESTAMP '2025-04-06 15:00:00.000000', 'Campionato Italiano Turismo', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(40, 'prove_libere', 60, TIMESTAMP '2024-03-01 09:00:00.000000', 'EURO GT4', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(41, 'qualifica', 45, TIMESTAMP '2024-03-02 11:00:00.000000', 'EURO GT4', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(42, 'gara', 90, TIMESTAMP '2024-03-03 14:00:00.000000', 'EURO GT4', 2024);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(43, 'prove_libere', 60, TIMESTAMP '2025-03-07 09:00:00.000000', 'EURO GT4', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(44, 'qualifica', 45, TIMESTAMP '2025-03-08 11:00:00.000000', 'EURO GT4', 2025);
+INSERT INTO Sessione (id, tipo, durata_min, data_ora, nome_campionato, anno_campionato) VALUES(45, 'gara', 90, TIMESTAMP '2025-03-09 14:00:00.000000', 'EURO GT4', 2025);
