551cd1337b8ac2c0693896c16304cb97d1a6da20
sungvzer <sungvzer@proton.me>
2024-07-08 21:16:10 +0200

    fix: make Passaggio.data_ora scale 4

--- up/109-Passaggio.sql
+++ up/109-Passaggio.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Passaggio (
     settore_sensore NUMBER,
-    data_ora TIMESTAMP,
+    data_ora TIMESTAMP(4),
     numero_auto NUMBER,
     sessione_id NUMBER NOT NULL,
     PRIMARY KEY (settore_sensore, data_ora)
