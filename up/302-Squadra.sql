b6c070b68f958654e3eba248dab09722030d3a92
sungvzer <sungvzer@proton.me>
2024-07-08 19:30:22 +0200

    fix: add missing teams

--- up/302-Squadra.sql
+++ up/302-Squadra.sql
@@ -24,3 +24,24 @@ VALUES('Toyota Gazoo Racing', 'Giappone', 2007, 'Rosso');
 
 INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
 VALUES('Audi Sport Team Joest', 'Germania', 1978, 'Rosso');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('AF Corse', 'Italia', 2006, 'Rosso');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('Lamborghini Squadra Corse', 'Italia', 2013, 'Giallo');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('Bronze Racing', 'Regno Unito', 2011, 'Bronzo');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('Silver Racing', 'Regno Unito', 2013, 'Argento');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('Gold Racing', 'Regno Unito', 2015, 'Oro');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('Volvo Polestar Racing', 'Svezia', 1996, 'Blu');
+
+INSERT INTO Squadra (nome, nazione, anno_fondazione, colore)
+VALUES('Subaru Tecnica International', 'Giappone', 1988, 'Blu');
